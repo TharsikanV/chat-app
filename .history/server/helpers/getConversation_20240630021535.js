d3620c4d6c430922c8b9d79a3b9dab4d4ad73d0d
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2024-06-30 03:42:12 +0530

    display the recently message reciever and sender with message count

---
+++ .history/server/helpers/getConversation_20240630021535.js
@@ -0,0 +1,34 @@
+import { ConversationModel } from "../models/ConversationModel"
+
+const getConversation =async (currentUserId)=>{
+    if(currentUserId){
+
+        const currentUserConversation = await ConversationModel.find({
+            "$or" : [
+                {sender : currentUserId},
+                {receiver : currentUserId}
+            ]
+        }).sort({updatedAt : -1}).populate('messages').populate('sender').populate('receiver')
+
+        console.log('currentUserConversation',currentUserConversation)
+
+        const conversation = currentUserConversation.map((conv)=>{
+
+            const countUnseenMsg = conv.messages.reduce((preve,curr)=>preve + (curr.seen ? 0 : 1),0)
+            return{
+                _id : conv?._id,
+                sender : conv?.sender,
+                receiver : conv?.receiver,
+                unseenMsg : countUnseenMsg,
+                lastMsg : conv.messages[conv?.messages?.length - 1]
+            }
+        })
+            // socket.emit('conversation',conversation)
+            return conve
+    }
+    else{
+        return[]
+    }
+}
+
+module.exports =getConversation
---
+++ .history/server/socket/index_20240629015812.js
@@ -0,0 +1,147 @@
+const express = require('express')
+const { Server } = require('socket.io')
+const http = require('http')
+const getUserDetailsFromToken = require('../helpers/getUserDetailsFromToken')
+const UserModel = require('../models/UserModel')
+const { profile } = require('console')
+const { ConversationModel, MessageModel } = require('../models/ConversationModel')
+
+const app = express()
+
+// socket connection
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL,
+        credentials: true
+    }
+})
+
+// socket running at http://localhost:8080/
+
+
+// online user
+const onlineUser = new Set()
+
+io.on('connection', async (socket) => {
+    console.log("connect User", socket.id)
+
+    const token = socket.handshake.auth.token
+
+    // current user details
+    const user = await getUserDetailsFromToken(token)
+
+    // create a room 
+    socket.join(user?._id.toString())
+    onlineUser.add(user?._id?.toString())
+
+    io.emit('onlineUser', Array.from(onlineUser))
+
+    socket.on('message-page', async (userId) => {
+        console.log('userId', userId)
+        const userDetails = await UserModel.findById(userId).select("-password")
+
+        const payload = {
+            _id: userDetails?._id,
+            name: userDetails?.name,
+            email: userDetails?.email,
+            profile_pic: userDetails?.profile_pic,
+            online: onlineUser.has(userId)
+        }
+        socket.emit('message-user', payload)
+
+         //get  previous message
+         const getConversationMessage =await ConversationModel.findOne({
+            "$or": [
+                { sender: user?._id, receiver: userId },
+                { sender: userId, receiver:user?._id},
+            ]
+        }).populate('messages').sort({updatedAt : -1})//-1 will be the decreasing order
+
+
+        // io.to(data?.sender).emit('message',getConversationMessage.messages)
+        // io.to(data?.receiver).emit('message',getConversationMessage.messages)
+
+        socket.emit('message',getConversationMessage.messages)
+         
+    })
+
+   
+
+
+    // new message
+    socket.on('new message', async (data) => {
+
+        // check conversation is available both user
+        let conversation = await ConversationModel.findOne({
+            "$or": [
+                { sender: data?.sender, receiver: data?.receiver },
+                { sender: data?.receiver, receiver: data?.sender },
+            ]
+        })
+       
+        // if conversation is not available 
+        if (!conversation) {
+            const createConversation = await ConversationModel({
+                sender: data?.sender,
+                receiver: data?.receiver
+            })
+
+            conversation = await createConversation.save()
+        }
+
+        const message = new MessageModel({
+            text: data.text,
+            imageUrl: data.imageUrl,
+            videoUrl: data.videoUrl,
+            msgByUserId: data?.msgByUserId,
+        })
+
+        const saveMessage=await message.save()
+
+        const updateConversation=await ConversationModel.updateOne({ _id:conversation._id},{
+            "$push" : { messages : saveMessage?._id}
+        })
+
+        
+    })
+
+    // sidebar
+    socket.on('sidebar',async (currentUserId)=>{
+        console.log("current user",currentUserId)
+
+        const currentUserConversation = await ConversationModel.find({
+            "$or" : [
+                {sender : currentUserId},
+                {receiver : currentUserId}
+            ]
+        }).sort({updatedAt : -1}).populate('messages').populate('sen')
+
+        console.log('currentUserConversation',currentUserConversation)
+
+        const conversation = currentUserConversation.map((conv)=>{
+
+            const countUnseenMsg = conv.messages.reduce((preve,curr)=>preve + (curr.seen ? 0 : 1),0)
+            return{
+                _id : conv?._id,
+                sender : conv?.sender,
+                receiver : conv?.receiver,
+                unseenMsg : countUnseenMsg,
+                lastMsg : conv.messages[conv?.messages?.length - 1]
+            }
+        })
+
+        socket.emit('conversation',conversation)
+    })
+
+    // disconnect
+    socket.on('disconnect', () => {
+        onlineUser.delete(user?._id)//user ah delete pannanum 
+        console.log('disconnect user', socket.id)
+    })
+})
+
+module.exports = {
+    app,
+    server
+}
